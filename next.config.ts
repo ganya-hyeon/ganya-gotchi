da458d90659d084d2ad80fcb1cde9bca587f7f3b
ganya-hyeon <ganya-hyeon@example.com>
2026-05-02 16:14:02 +0900

    fix: optimize next config for sanity studio deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
   },
 };
 
